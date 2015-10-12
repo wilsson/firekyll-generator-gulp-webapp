9e19ff0f4f68155a6c720b5ec01fec414fa82019
wilsson <wilsonft83@gmail.com>
2015-10-12 18:33:54 -0500

    feat(path):agregando examples y ruta para indicar la creacion

--- app/index.js
+++ app/index.js
@@ -1,37 +1,44 @@
-function generator(){
+/**
+ *
+ * @module Generator
+ * @author Wilson Flores
+ *
+ */
+
+function Generator(){
   this.plugins = require('./plugins');
 }
 
-generator.prototype.directory = function(){
+Generator.prototype.directory = function(path){
   var ctx = this;
-  this.plugins.mkdirp('./frontend/gulpConfig/tasks', function (err) {
+  this.plugins.mkdirp(this.plugins.path.join(path,'/frontend/gulpConfig/tasks'), function (err) {
     if (err) console.error(err)
     else{
-      ctx.tasksGulp();
-      ctx.gulpfile();
+      ctx.tasksGulp(path);
+      ctx.gulpfile(path);
     }
   });
 }
 
-generator.prototype.tasksGulp = function(){
+Generator.prototype.tasksGulp = function(path){
   var _tasks = this.plugins.fs.readdirSync(this.plugins.path.join(__dirname,'/templates/tasks'));
   var input = this.plugins.path.join(__dirname,'/templates/tasks');
-  var output = this.plugins.path.join('frontend','gulpConfig','tasks');
+  var output = this.plugins.path.join(path,'frontend','gulpConfig','tasks');
   this.files(_tasks,input,output);
 }
 
-generator.prototype.gulpfile = function(){
+Generator.prototype.gulpfile = function(path){
   var _tasks = this.plugins.fs.readdirSync(this.plugins.path.join(__dirname,'/templates/gulp'));
   var input = this.plugins.path.join(__dirname,'/templates/gulp');
-  var output = this.plugins.path.join('frontend');
+  var output = this.plugins.path.join(path,'frontend');
   this.files(_tasks,input,output);
 }
 
-generator.prototype.files = function(_tasks,input,output){
+Generator.prototype.files = function(_tasks,input,output){
   for(var i=0;i<_tasks.length;i++){
     this.plugins.fs.createReadStream(this.plugins.path.join(input,_tasks[i])).pipe
       (this.plugins.fs.createWriteStream(this.plugins.path.join(output,_tasks[i])));
   }
 }
 
-module.exports = generator;
+module.exports = Generator;
